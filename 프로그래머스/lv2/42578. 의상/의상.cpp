4d3dc5dc8a871676de07f9d8e325c4a23a0adf8e
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-11 19:20:45 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> m;
+    
+    for(auto cloth: clothes) {
+        m[cloth[1]] += 1;
+    }
+    
+    for(auto it: m) {
+        answer *= (it.second + 1);
+    }
+    answer--;
+    
+    return answer;
+}
