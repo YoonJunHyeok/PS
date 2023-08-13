06315546385e8fbd5ba395acd710b729324fe99c
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-13 18:01:20 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -25,20 +25,12 @@ void unify(int x, int y) {
     else board[x] = y;
 }
 
-bool check() {
-    for(int i = 0; i < board.size() - 1; i++) {
-        if(find(board[i]) != find(board[i + 1])) return false;
-    }
-    return true;
-}
-
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
     for(int i = 0; i < n; i++) board.push_back(i);
     sort(costs.begin(), costs.end(), cmp);
     
     for(vector<int> cost: costs) {
-        if(check()) break;
         if(find(cost[0]) == find(cost[1])) continue;
         unify(cost[0], cost[1]);
         answer += cost[2];
