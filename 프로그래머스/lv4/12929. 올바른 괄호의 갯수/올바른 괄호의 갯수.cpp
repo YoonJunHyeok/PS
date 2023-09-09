7e4eab2b170bf4635e5b061dd173c1b2c62c3f34
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-09-09 19:22:15 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            dp[i] += dp[j] * dp[i - j - 1];
+        }
+    }
+    answer = dp[n];
+    return answer;
+}
