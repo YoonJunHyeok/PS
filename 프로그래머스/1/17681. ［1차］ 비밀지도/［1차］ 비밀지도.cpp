e1be7e2163a53072153d12c243baf5d296754b32
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-11-19 15:21:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -9,18 +9,15 @@ vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
     for(int i = 0; i < n; i++) {
         string tmp = "";
         
-        int n1 = arr1[i], n2 = arr2[i];
+        int num = arr1[i] | arr2[i];
         
         for(int j = 0; j < n; j++) {
-            if(!(n1 & 1) && !(n2 & 1)) {
-                tmp = " " + tmp;
-            }
-            else {
+            if(num & 1) 
                 tmp = "#" + tmp;
-            }
+            else 
+                tmp = " " + tmp;
             
-            n1 = n1 >> 1;
-            n2 = n2 >> 1;
+            num = num >> 1;
         }
         
         answer.push_back(tmp);
