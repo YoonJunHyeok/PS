a7ce5bb4178d4f92782dda4eb5dce9a49baa9d04
Junhyeok Yoon <jhyoon1607@gmail.com>
2024-03-12 17:12:29 +0900

    [level 2] Title: 호텔 대실, Time: 0.26 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<string>> book_time) {
+    int answer = 0;
+    
+    vector<int> cnt(2410, 0);
+    for(vector<string> tm: book_time) {
+        int start_time = stoi(tm[0].substr(0, 2) + tm[0].substr(3, 5));
+        int end_time = stoi(tm[1].substr(0, 2) + tm[1].substr(3, 5));
+        if(end_time % 100 < 50) end_time += 10;
+        else end_time = end_time + 100 - 50;
+        
+        for(int idx = start_time; idx < end_time; idx++) {
+            cnt[idx]++;
+        }
+    }
+    
+    answer = *max_element(cnt.begin(), cnt.end());
+    
+    return answer;
+}
