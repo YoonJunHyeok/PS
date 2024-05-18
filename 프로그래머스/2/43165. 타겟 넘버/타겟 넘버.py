41a12654ab8fe861f5d7208ae013c96840fc2578
Junhyeok Yoon <jhyoon1607@gmail.com>
2024-05-18 22:57:43 +0900

    [level 2] Title: 타겟 넘버, Time: 1.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,7 @@
-ans = 0
+from itertools import product
 
-def dfs(numbers, target, idx, cur_sum):
-    global ans
-    
-    if idx == len(numbers):
-        if cur_sum == target:
-            ans += 1
-        return
-
-    dfs(numbers, target, idx + 1, cur_sum + numbers[idx])
-    dfs(numbers, target, idx + 1, cur_sum - numbers[idx])
-    
 def solution(numbers, target):
-    dfs(numbers, target, 0, 0)
+    all_nums = [(x, -x) for x in numbers]
+    possible_values = list(map(sum, product(*all_nums)))
     
-    return ans
+    return possible_values.count(target)
