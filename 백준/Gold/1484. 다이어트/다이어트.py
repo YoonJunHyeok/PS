00de2516aa88d600dd5e98a7f39915cc54694052
Junhyeok Yoon <jhyoon1607@gmail.com>
2024-06-06 23:11:22 +0900

    [Gold V] Title: 다이어트, Time: 44 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.py
@@ -0,0 +1,24 @@
+# x^2 - y^2 = G
+# (x+y)(x-y) = G = a * b
+# x = (a + b) / 2
+import sys
+import math
+input = sys.stdin.readline
+
+G = int(input())
+
+ans = []
+for a in range(1, math.ceil(math.sqrt(G))):
+    if G % a == 0:
+        b = int(G / a)
+
+        if a == b or (a + b) % 2 == 1:
+            continue
+        ans.append(int((a + b) / 2))
+
+if not ans:
+    print(-1)
+else:
+    ans.sort()
+    for n in ans:
+        print(n)
