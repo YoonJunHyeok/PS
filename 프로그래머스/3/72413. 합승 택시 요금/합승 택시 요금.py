79c7f45b822abddf4900d03424d84e025c5f8b8e
Junhyeok Yoon <jhyoon1607@gmail.com>
2024-10-05 00:53:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 1367.10 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,19 +1,14 @@
+from itertools import product
+
 def solution(n, s, a, b, fares):
     graph = [[20000001] * (n + 1) for _ in range(n + 1)]
     for idx in range(n + 1):
         graph[idx][idx] = 0
     for i, j, cost in fares:
-        graph[i][j] = cost
-        graph[j][i] = cost
+        graph[i][j] = graph[j][i] = cost
         
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                if graph[i][j] > graph[i][k] + graph[k][j]:
-                    graph[i][j] = graph[i][k] + graph[k][j]
-
-    answer = graph[s][a] + graph[s][b]
-    for k in range(1, n + 1):
-        answer = min(answer, graph[s][k] + graph[k][a] + graph[k][b])
+    for k, i, j in product(range(1, n + 1), repeat=3):
+        if graph[i][j] > graph[i][k] + graph[k][j]:
+            graph[i][j] = graph[i][k] + graph[k][j]
                 
-    return answer
+    return min(graph[s][k] + graph[k][a] + graph[k][b] for k in range(1, n + 1))
