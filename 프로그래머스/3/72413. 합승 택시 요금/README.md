a351cdb92ebd31b02c0735ced9413eb39acfa126
Junhyeok Yoon <jhyoon1607@gmail.com>
2025-03-15 22:03:03 +0900

    [level 3] Title: 합승 택시 요금, Time: 3713.33 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 13 MB, 시간: 1367.10 ms
+메모리: 11.9 MB, 시간: 3713.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 00:53:01
+2025년 03월 15일 22:03:00
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,14 +1,25 @@
 from itertools import product
 
-def solution(n, s, a, b, fares):
-    graph = [[20000001] * (n + 1) for _ in range(n + 1)]
-    for idx in range(n + 1):
-        graph[idx][idx] = 0
-    for i, j, cost in fares:
-        graph[i][j] = graph[j][i] = cost
-        
-    for k, i, j in product(range(1, n + 1), repeat=3):
-        if graph[i][j] > graph[i][k] + graph[k][j]:
-            graph[i][j] = graph[i][k] + graph[k][j]
-                
-    return min(graph[s][k] + graph[k][a] + graph[k][b] for k in range(1, n + 1))
+def solution(n, s, a, b, fares):    
+    costs = [[-1] * n for _ in range(n)]
+    for i in range(n):
+        costs[i][i] = 0
+    for c, d, f in fares:
+        costs[c - 1][d - 1] = costs[d - 1][c - 1] = f
+    
+    for mid, start, end in product(range(n), repeat=3):
+        if costs[start][mid] == -1 or costs[mid][end] == -1:
+            continue
+            
+        if costs[start][end] == -1:
+            costs[start][end] = costs[start][mid] + costs[mid][end]
+        else:
+            costs[start][end] = min(costs[start][end], costs[start][mid] + costs[mid][end])
+            
+    answer = costs[s - 1][a - 1] + costs[s - 1][b - 1]
+    for mid in range(n):
+        if costs[s - 1][mid] == -1 or costs[mid][a - 1] == -1 or costs[mid][b - 1] == -1:
+            continue
+        answer = min(answer, costs[s - 1][mid] + costs[mid][a - 1] + costs[mid][b - 1])
+    
+    return answer
